45c394d6aef576ccbc146be6d8b79ffdf66fce5f
WishesFire <vmaksimus706@gmail.com>
2021-09-16 23:30:29 +0300

    add full ref design

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+import os
+
 GUI_SIZE_X = 400
 GUI_SIZE_Y = 150
 
 KEYBOARD_COMBO = ['~', '`', '₴', "'", "ё", "Ё"]
+
+BASE_DIR = os.path.abspath(os.curdir)
--- gui/design.py
+++ gui/design.py
@@ -1,5 +1,5 @@
-from PyQt6 import QtWidgets, QtCore
-from config import GUI_SIZE_X, GUI_SIZE_Y
+from PyQt6 import QtWidgets, QtCore, QtGui
+from config import GUI_SIZE_X, GUI_SIZE_Y, BASE_DIR
 
 
 class MyGuiWindow(QtWidgets.QWidget):
@@ -11,6 +11,74 @@ class MyGuiWindow(QtWidgets.QWidget):
             QtCore.Qt.WindowStaysOnTopHint)
         self.setStyleSheet('background-color: #B0E0E6;')
         self.resize(GUI_SIZE_X, GUI_SIZE_Y)
-        self.button_list = []
         box = QtWidgets.QVBoxLayout()
         self.setLayout(box)
+
+        self.clipboard = QtWidgets.QApplication.clipboard()
+        self.clipboard.dataChanged.connect(self.detect_clipboard_duplication)
+
+        self.scroll = QtWidgets.QScrollArea()
+        self.hbox.addWidget(self.scroll)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setFixedHeight(150)
+        self.scrollContent = QtWidgets.QWidget(self.scroll)
+        self.scroll_layout = QtWidgets.QVBoxLayout(self.scrollContent)
+        self.scrollContent.setLayout(self.scroll_layout)
+        self.scroll.setWidget(self.scrollContent)
+
+        # Icon
+        self.setWindowIcon(QtGui.QIcon(BASE_DIR + '\\logo\\Icon2.png'))
+
+        self.tray = QtWidgets.QSystemTrayIcon(self)
+        self.tray.setIcon(QtGui.QIcon(BASE_DIR + '\\logo\\Icon2.png'))
+
+        show_action = QtWidgets.QAction('Show', self)
+        quit_action = QtWidgets.QAction('Exit', self)
+        hide_action = QtWidgets.QAction('Hide', self)
+        inform_action = QtWidgets.QAction('Information', self)
+
+        show_action.triggered.connect(self.show)
+        hide_action.triggered.connect(self.hide)
+        inform_action.triggered.connect(self.info)
+        quit_action.triggered.connect(QtWidgets.qApp.quit)
+
+        tray_menu = QtWidgets.QMenu()
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(hide_action)
+        tray_menu.addAction(inform_action)
+        tray_menu.addAction(quit_action)
+
+        self.tray.setContextMenu(tray_menu)
+        self.tray.show()
+
+    def detect_clipboard_duplication(self):
+        out = ''
+        total_part = 50
+        total_word = len(self.clipboard.text())
+
+        if total_word > total_part:
+            for part in range(total_word // total_part):
+                out += f"{part[:total_part]}\n{part[total_part:]}"
+            out += "\n"
+        else:
+            out = self.clipboard.text()
+
+        button = QtWidgets.QPushButton(out)
+        button.clicked.connect(self.button_func)
+        self.scrolllayout.addWidget(self.button)
+
+    def button_func(self):
+        sender = self.sender()
+        self.clipboard.setText(sender.text())
+        self.hide()
+
+    def info(self):
+        QtWidgets.QMessageBox.about(self, "About program",
+                                          "<center>\"CopyPaster\" v2.0<br><br>"
+                                          "A program for viewing your copy list and using them<br><br>"
+                                          "(c) Vojtsitskiy Maksym, 2020")
+        self.show()
+
+    def closeEvent(self, evt):
+        evt.ignore()
+        self.hide()
